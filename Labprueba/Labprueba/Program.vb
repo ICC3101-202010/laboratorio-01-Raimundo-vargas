81e6e38b1bd3c08c827589aa7944f09feaf88423
Raimundo Vargas Alcalde <rjvargas@miuandes.cl>
2020-03-16 20:22:50 -0300

    cambio 2

--- Labprueba/Labprueba/Program.vb
+++ Labprueba/Labprueba/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("al fin finciona esta wea")
+        Console.WriteLine("al fin finciona esto")
     End Sub
 End Module
