30109692ac79517f893daef827a21e3a5774e353
Raimundo Vargas Alcalde <rjvargas@miuandes.cl>
2020-03-17 15:15:52 -0300

    avances antes del lab

--- Laboratorio 1/Laboratorio 1/Program.cs
+++ Laboratorio 1/Laboratorio 1/Program.cs
@@ -2,13 +2,30 @@
 
 namespace Laboratorio_1
 {
-    class Program
+    class Pers
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
+            {
+                Persona Pers = new Persona();
+                Pers.Apellido = "Vargas";
+                Console.WriteLine("Apellido: {0}",Pers.Apellido);
+
+            }
+    }
+    
+    class Persona
+    {
+        //atributos datos
+        public string Nombre;
+        public string Apellido;
+        //metodos funciones
+        public void lanzar()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("hola");
 
         }
+
+
     }
+    
+
 }
