729404a225a1c34624bcc6009fcd04c481dd0aab
Raimundo Vargas Alcalde <rjvargas@miuandes.cl>
2020-03-17 17:48:45 -0300

    Lab

--- Laboratorio 1/Laboratorio 1/Program.cs
+++ Laboratorio 1/Laboratorio 1/Program.cs
@@ -6,24 +6,17 @@ namespace Laboratorio_1
     {
         static void Main()
             {
-
                 Person persona = new Person("Bob", "kunga");
                 //Jugador 1
                 int jugada = 0;
                 jugada = persona.Lanzar();
                 Console.WriteLine(jugada);
-
-
-
             }
-    }
-    
-   
+    }  
     class Person
     {
         private string last;
         private string first;
-
         public Person(string lastName, string firstName)
         {
             last = lastName;
@@ -35,7 +28,6 @@ namespace Laboratorio_1
             Random rnd = new Random();
             int a = rnd.Next(0, 3);
             return a;
-
         }
 
         // Remaining implementation of Person class.
